6819b8e441dfb6a74642934b8eee89be5f72910b
stmacebal <mateo.cebquerol@gmail.com>
2023-12-01 21:49:23 +0100

    implemented print function

--- 3.1_ComplexNumbersOperatorOverloading/myCode/CComplex.cpp
+++ 3.1_ComplexNumbersOperatorOverloading/myCode/CComplex.cpp
@@ -6,14 +6,15 @@
  */
 
 #include "CComplex.h"
+#include <iostream>
 
 CComplex::CComplex(float real, float imaginary)
 {
-	if(real < - 100 || real > 100)
+	if(real < -100 || real > 100)
 	{
 		real = 0;
 	}
-    if(imaginary < - 100 || imaginary > 100)
+    if(imaginary < -100 || imaginary > 100)
 	{
 		imaginary = 0;
 	}
@@ -28,13 +29,13 @@ CComplex::CComplex(float real, float imaginary)
 CComplex::CComplex(const CComplex &c)
 {
 
-	if(c.m_real < - 100 || c.m_real > 100)
+	if(c.m_real < -100 || c.m_real > 100)
 	{
-		c.m_real = 0;
+		m_real = 0;
 	}
-    if(c.m_imaginary < - 100 || c.m_imaginary > 100)
+    if(c.m_imaginary < -100 || c.m_imaginary > 100)
 	{
-    	c.m_imaginary = 0;
+    	m_imaginary = 0;
 	}
 
     m_real = c.m_real;
@@ -45,25 +46,44 @@ CComplex::CComplex(const CComplex &c)
 
 void CComplex::set(float real, float imaginary)
 {
+	if(real < -100 || real > 100)
+	{
+		real = 0;
+	}
+    if(imaginary < -100 || imaginary > 100)
+	{
+		imaginary = 0;
+	}
+
+    m_real = real;
+    m_imaginary = imaginary;
 
 }
 
 float CComplex::abs()
 {
+	if(m_real < 0)
+	{
+		return m_real * -1;
+	}
+	else
+	{
+		return m_real;
+	}
 
 }
 
 void CComplex::print() const
 {
-
+	std::cout << m_real << " + " << m_imaginary << "i" << std::endl;
 }
 
-CComplex& CComplex::operator ++()
-{
-
-}
-
-CComplex CComplex::operator ++(int)
-{
-
-}
+//CComplex& CComplex::operator ++()
+//{
+//
+//}
+//
+//CComplex CComplex::operator ++(int)
+//{
+//
+//}
