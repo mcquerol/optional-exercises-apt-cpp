979e499a69fa594545f52801df7ab9d34e7b4130
stmacebal <mateo.cebquerol@gmail.com>
2023-12-01 12:41:11 +0100

    created CSetOfMeasurementValues class

---
+++ 2.2_SetOfMeasurementValues/myCode/CCSetOfMeasurementValues.cpp
@@ -0,0 +1,9 @@
+/*
+ * CCSetOfMeasurementValues.cpp
+ *
+ *  Created on: 1 Dec 2023
+ *      Author: robom
+ */
+
+#include "CCSetOfMeasurementValues.h"
+
---
+++ 2.2_SetOfMeasurementValues/myCode/CCSetOfMeasurementValues.h
@@ -0,0 +1,15 @@
+/*
+ * CCSetOfMeasurementValues.h
+ *
+ *  Created on: 1 Dec 2023
+ *      Author: robom
+ */
+
+#ifndef CCSETOFMEASUREMENTVALUES_H_
+#define CCSETOFMEASUREMENTVALUES_H_
+
+class CCSetOfMeasurementValues
+{
+};
+
+#endif /* CCSETOFMEASUREMENTVALUES_H_ */
