b5b6456abea0e44056d0b665f5463cfa1b8f3730
stmacebal <mateo.cebquerol@gmail.com>
2023-12-01 23:26:37 +0100

    added operator overload declarations

--- 3.1_ComplexNumbersOperatorOverloading/myCode/CComplex.h
+++ 3.1_ComplexNumbersOperatorOverloading/myCode/CComplex.h
@@ -10,20 +10,36 @@
 
 class CComplex
 {
-	private:
-		float m_real;
-		float m_imaginary;
+private:
+    float m_real;
+    float m_imaginary;
 
-	public:
-		CComplex(float real = 0, float imaginary = 0);
-		CComplex(CComplex const& c);
+public:
+    CComplex(float real = 0, float imaginary = 0);
+    CComplex(const CComplex &c);
 
-		void set(float real, float imaginary);
-		float abs();
-		void print() const;
-		CComplex& operator++();
-		CComplex operator++(int);
+    void set(float real, float imaginary);
+    float abs() const; // Changed to const
+    void print() const;
+    CComplex &operator++();
+    CComplex operator++(int);
 
+    // Overloaded operators
+    CComplex operator+(const CComplex &other) const;
+    CComplex operator+(float value) const;
+    friend CComplex operator+(float value, const CComplex &complex);
+
+    CComplex operator-(const CComplex &other) const;
+    CComplex operator-(float value) const;
+    friend CComplex operator-(float value, const CComplex &complex);
+
+    CComplex operator*(const CComplex &other) const;
+    CComplex operator*(float value) const;
+    friend CComplex operator*(float value, const CComplex &complex);
+
+    CComplex operator/(const CComplex &other) const;
+    CComplex operator/(float value) const;
+    friend CComplex operator/(float value, const CComplex &complex);
 };
 
 #endif /* CCOMPLEX_H_ */
