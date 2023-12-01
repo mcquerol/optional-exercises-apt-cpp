10dc45484de9c0752e48a023722c1e459d4f27f4
stmacebal <mateo.cebquerol@gmail.com>
2023-12-01 12:33:05 +0100

    added test cases in the main and TODO

--- 2.1_Coordinate/myCode/main.cpp
+++ 2.1_Coordinate/myCode/main.cpp
@@ -21,5 +21,15 @@ int main ()
 	c2.print(CCoordinate::POLAR);
 	c3.print(CCoordinate::POLAR);
 
+	c1.add(c3);
+	c2.add(CCoordinate(1,2,3));
+	c1.print(CCoordinate::CARTESIAN);
+	c2.print(CCoordinate::CARTESIAN);
+
+	/* TODO add illegal cases to prevent nan (show warnining and set val to 0)
+	 * change output to make it more clear in the console
+	 * add doxygen comments
+	 */
+
 	return 0;
 }
