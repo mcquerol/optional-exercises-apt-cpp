60c3d8f1cbfd48ea661a2827bf2ff9b3321100b4
stmacebal <mateo.cebquerol@gmail.com>
2023-12-01 12:09:33 +0100

    added coorindate

---
+++ 2.1_Coordinate/myCode/CCoordinate.h
@@ -0,0 +1,45 @@
+/*
+ * CCoordinate.h
+ *
+ *  Created on: 1 Dec 2023
+ *      Author: robom
+ */
+
+#ifndef CCOORDINATE_H_
+#define CCOORDINATE_H_
+
+#include <math.h>
+#include <iostream>
+
+#define M_PI 3.14159265358979323846 /* pi */
+
+class CCoordinate
+{
+private:
+	float m_x;
+	float m_y;
+	float m_z;
+
+	void getPolar(float &r, float &phi, float &theta);
+	void getCylinder(float &r, float &phi, float &h);
+
+public:
+
+	enum t_coordinate
+	{
+		CARTESIAN,
+		CYLINDER,
+		POLAR
+	};
+
+	CCoordinate(float x = 0, float y = 0, float z = 0);
+	~CCoordinate();
+
+	void getCartesian(float &x, float &y, float &z);
+	void setCartesian(float x, float y, float z);
+
+	void add(CCoordinate& c);
+	void print(t_coordinate style);
+};
+
+#endif /* CCOORDINATE_H_ */
