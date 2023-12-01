ecd780f5c309e1c2043bc6b60cc9432c3c8ff09e
stmacebal <mateo.cebquerol@gmail.com>
2023-12-01 15:58:46 +0100

    added test case to test out of bounds

--- 2.2_SetOfMeasurementValues/myCode/main.cpp
+++ 2.2_SetOfMeasurementValues/myCode/main.cpp
@@ -10,6 +10,13 @@ int main ()
 	cout << "2.2_SetOfMeasurementValues started." << endl << endl;
 
 	CSetOfMeasurementValues myVoltageValues("myVoltageValues",CSetOfMeasurementValues::V);
+	myVoltageValues.setValue(1,10);
+	myVoltageValues.setValue(3,20);
+	myVoltageValues.print();
+
+	myVoltageValues.setValue(30, 20);
+	myVoltageValues.getValue(77);
+
 
 	return 0;
 }
