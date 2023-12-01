ccae5fcf7e07c0ae21bb0c52b97165185700a79b
stmacebal <mateo.cebquerol@gmail.com>
2023-12-01 12:52:47 +0100

    changed class names

---
+++ 2.2_SetOfMeasurementValues/myCode/CSetOfMeasurementValues.h
@@ -0,0 +1,15 @@
+/*
+ * CSetOfMeasurementValues.h
+ *
+ *  Created on: 1 Dec 2023
+ *      Author: robom
+ */
+
+#ifndef CSETOFMEASUREMENTVALUES_H_
+#define CSETOFMEASUREMENTVALUES_H_
+
+class CSetOfMeasurementValues
+{
+};
+
+#endif /* CSETOFMEASUREMENTVALUES_H_ */
