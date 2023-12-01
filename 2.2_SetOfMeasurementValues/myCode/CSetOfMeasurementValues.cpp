b7cd2588c5c1622f6b15c75e73c51e1a71b9239e
stmacebal <mateo.cebquerol@gmail.com>
2023-12-01 13:11:54 +0100

    implemented constructor and setValue

--- 2.2_SetOfMeasurementValues/myCode/CSetOfMeasurementValues.cpp
+++ 2.2_SetOfMeasurementValues/myCode/CSetOfMeasurementValues.cpp
@@ -7,8 +7,29 @@
 
 #include "CSetOfMeasurementValues.h"
 
+CSetOfMeasurementValues::CSetOfMeasurementValues(std::string name, t_unit unit)
+{
+	m_name = name;
+	m_unit = unit;
+
+	for(int i = 0; i < 10; i++)
+	{
+		m_value[i] = NOVALUE;
+	}
+
+	std::cout << "object created at " << this << std::endl;
+}
+
 void CSetOfMeasurementValues::setValue(unsigned int pos, double value)
 {
+	if(pos < 0 || pos > 10)
+	{
+		std::cerr << "OUT OF BOUNDS" << std::endl;
+	}
+	else
+	{
+		m_value[pos] = value;
+	}
 
 }
 
@@ -24,22 +45,22 @@ void CSetOfMeasurementValues::print() const
 
 double CSetOfMeasurementValues::getValue(unsigned int post) const
 {
-
+	return 0.0;
 }
 
 double CSetOfMeasurementValues::getAverage() const
 {
-
+	return 0.0;
 }
 
 double CSetOfMeasurementValues::getMax() const
 {
-
+	return 0.0;
 }
 
 double CSetOfMeasurementValues::getMin() const
 {
-
+	return 0.0;
 }
 
 void CSetOfMeasurementValues::printSingleValue(unsigned int pos) const
