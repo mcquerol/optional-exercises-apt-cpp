d660d043171a8b23aa8d9aa30a0d1aba48d39288
stmacebal <mateo.cebquerol@gmail.com>
2023-12-01 16:09:00 +0100

    created new object to test setRandomValues from 0 to 100

--- 2.2_SetOfMeasurementValues/myCode/main.cpp
+++ 2.2_SetOfMeasurementValues/myCode/main.cpp
@@ -8,7 +8,7 @@ int main ()
 {
     // TODO: Add your program code here
 	cout << "2.2_SetOfMeasurementValues started." << endl << endl;
-
+	srand(time(NULL));
 	CSetOfMeasurementValues myVoltageValues("myVoltageValues",CSetOfMeasurementValues::V);
 	myVoltageValues.setValue(1,10);
 	myVoltageValues.setValue(3,20);
@@ -17,6 +17,10 @@ int main ()
 	myVoltageValues.setValue(30, 20);
 	myVoltageValues.getValue(77);
 
+	CSetOfMeasurementValues myCurrentValues("myCurrentValues",CSetOfMeasurementValues::A);
+	myCurrentValues.setRandomValues(0,100);
+	myCurrentValues.print();
 
+	//TODO check the getmin function and output NOVALUE after the cerr
 	return 0;
 }
