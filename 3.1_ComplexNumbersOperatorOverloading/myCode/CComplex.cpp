4dc2c822ca66ce7df62379b395b69268ec08a0b7
stmacebal <mateo.cebquerol@gmail.com>
2023-12-01 21:22:17 +0100

    created ComplexNumber classes

---
+++ 3.1_ComplexNumbersOperatorOverloading/myCode/CComplex.cpp
@@ -0,0 +1,9 @@
+/*
+ * CComplex.cpp
+ *
+ *  Created on: 1 Dec 2023
+ *      Author: robom
+ */
+
+#include "CComplex.h"
+
---
+++ 3.1_ComplexNumbersOperatorOverloading/myCode/CComplex.h
@@ -0,0 +1,15 @@
+/*
+ * CComplex.h
+ *
+ *  Created on: 1 Dec 2023
+ *      Author: robom
+ */
+
+#ifndef CCOMPLEX_H_
+#define CCOMPLEX_H_
+
+class CComplex
+{
+};
+
+#endif /* CCOMPLEX_H_ */
