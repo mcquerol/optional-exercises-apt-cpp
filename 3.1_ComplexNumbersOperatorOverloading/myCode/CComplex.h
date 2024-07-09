d3cf520b7cf5173e239a31889e0ae87ce475aa8a
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-07-09 02:13:31 +0200

    added pre and post increment operator and also added ostream overload

--- 3.1_ComplexNumbersOperatorOverloading/myCode/CComplex.h
+++ 3.1_ComplexNumbersOperatorOverloading/myCode/CComplex.h
@@ -8,6 +8,8 @@
 #ifndef CCOMPLEX_H_
 #define CCOMPLEX_H_
 
+#include <iostream>
+
 class CComplex
 {
 private:
@@ -40,6 +42,8 @@ public:
     CComplex operator/(const CComplex &other) const;
     CComplex operator/(float value) const;
     friend CComplex operator/(float value, const CComplex &complex);
-};
 
+    friend std::ostream& operator<< (std::ostream& out, const CComplex& c);
+
+};
 #endif /* CCOMPLEX_H_ */
