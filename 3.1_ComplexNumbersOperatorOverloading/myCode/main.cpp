972ef7ed6a9f8ac7fcf1170047c815d249cb8e49
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-07-09 02:16:10 +0200

    added more test cases for complex numbers

--- 3.1_ComplexNumbersOperatorOverloading/myCode/main.cpp
+++ 3.1_ComplexNumbersOperatorOverloading/myCode/main.cpp
@@ -20,5 +20,18 @@ int main ()
 	c3.print();
 	CComplex c4(c3);
 	c4.print();
+
+	float x = 3.14;
+	c1 = c3+x;
+	cout << "c1: " << c1 << endl;
+	// Add more testcases as needed
+
+	CComplex c;
+	CComplex d;
+	d = ++c;
+	cout << "++c = " << c << "; d=++c = " << d << endl;
+	d.set(0,0);
+	d = c++;
+	cout << "c++ = " << c << "; d=c++ = " << d << endl;
 	return 0;
 }
