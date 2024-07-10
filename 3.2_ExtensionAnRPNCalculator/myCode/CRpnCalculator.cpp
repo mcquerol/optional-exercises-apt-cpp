f2340ee297b2a930f774597b8f009ffe5fb03936
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-07-10 21:16:38 +0200

    created CPrnCalculator class

---
+++ 3.2_ExtensionAnRPNCalculator/myCode/CRpnCalculator.cpp
@@ -0,0 +1,9 @@
+/*
+ * CRpnCalculator.cpp
+ *
+ *  Created on: 10 Jul 2024
+ *      Author: mateo
+ */
+
+#include "CRpnCalculator.h"
+
---
+++ 3.2_ExtensionAnRPNCalculator/myCode/CRpnCalculator.h
@@ -0,0 +1,15 @@
+/*
+ * CRpnCalculator.h
+ *
+ *  Created on: 10 Jul 2024
+ *      Author: mateo
+ */
+
+#ifndef CRPNCALCULATOR_H_
+#define CRPNCALCULATOR_H_
+
+class CRpnCalculator
+{
+};
+
+#endif /* CRPNCALCULATOR_H_ */
