de8cfa31c3f1538b3bbf2c74f2f842a2774f0a6d
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-07-11 00:08:45 +0200

    added method definitions for operations

--- 3.2_ExtensionAnRPNCalculator/myCode/CRpnCalculator.cpp
+++ 3.2_ExtensionAnRPNCalculator/myCode/CRpnCalculator.cpp
@@ -7,3 +7,75 @@
 
 #include "CRpnCalculator.h"
 
+bool CRpnCalculator::pushValue(const CFraction &data)
+{
+	return m_stack.push(data);
+}
+
+bool CRpnCalculator::popValue(CFraction &data)
+{
+	return m_stack.pop(data);
+}
+
+void CRpnCalculator::add()
+{
+	CFraction f1, f2, sum;
+	if(m_stack.pop(f1) && m_stack.pop(f2))
+	{
+		sum = f1 + f2;
+		m_stack.push(sum);
+	}
+	else
+	{
+		std::cerr << "ERROR, buffer empty" << std::endl;
+	}
+}
+
+void CRpnCalculator::subtract()
+{
+	CFraction f1, f2, sum;
+	if(m_stack.pop(f1) && m_stack.pop(f2))
+	{
+		sum = f2 - f1; //order matters
+		m_stack.push(sum);
+	}
+	else
+	{
+		std::cerr << "ERROR, buffer empty" << std::endl;
+	}
+}
+
+void CRpnCalculator::multiply()
+{
+	CFraction f1, f2, sum;
+	if(m_stack.pop(f1) && m_stack.pop(f2))
+	{
+		sum = f1 * f2; //order matters
+		m_stack.push(sum);
+	}
+	else
+	{
+		std::cerr << "ERROR, buffer empty" << std::endl;
+	}
+}
+
+void CRpnCalculator::divide()
+{
+	CFraction f1, f2, sum;
+	if(m_stack.pop(f1) && m_stack.pop(f2))
+	{
+		if(f1.toDouble() != 0.0)
+		{
+			sum = f2 / f1;
+			m_stack.push(sum);
+		}
+		else
+		{
+			std::cerr << "ERROR, cannot divide by 0" << std::endl;
+		}
+	}
+	else
+	{
+		std::cerr << "ERROR, buffer empty" << std::endl;
+	}
+}
