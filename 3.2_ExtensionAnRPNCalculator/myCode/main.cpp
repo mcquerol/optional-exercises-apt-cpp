e680e70c8b808bd62a9d8c7dc6ac3f995ab3995c
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-07-10 21:00:21 +0200

    added test cases for the buffer

--- 3.2_ExtensionAnRPNCalculator/myCode/main.cpp
+++ 3.2_ExtensionAnRPNCalculator/myCode/main.cpp
@@ -5,15 +5,30 @@
 // #include <string>
 
 using namespace std;
+#include "CLifoBuffer.h"
 
-// Add your project's header files here
-// #include "CFraction.h"
-
-// Main program
 int main ()
 {
     // TODO: Add your program code here
 	cout << "3.2_ExtensionAnRPNCalculator started." << endl << endl;
+
+	    CLifoBuffer buffer(5); // Create a buffer with 5 elements
+
+	    CFraction f1(1, 2);
+	    CFraction f2(3, 4);
+	    CFraction f3(5, 6);
+
+	    buffer.push(f1);
+	    buffer.push(f2);
+	    buffer.push(f3);
+
+	    buffer.print();
+
+	    CFraction popResult;
+	    buffer.pop(popResult);
+	    std::cout << "Popped: " << popResult << std::endl;
+
+	    buffer.print();
 
 	return 0;
 }
