c8971919b8c26f93184fb0b9729c4110d7e885c4
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-07-11 15:04:50 +0200

    added test cases for rpn calculator

--- 3.2_ExtensionAnRPNCalculator/myCode/main.cpp
+++ 3.2_ExtensionAnRPNCalculator/myCode/main.cpp
@@ -1,16 +1,15 @@
-// Standard (system) header files
+
 #include <iostream>
-#include <cstdlib>
-// Add more standard header files as required
-// #include <string>
 
 using namespace std;
+
 #include "CLifoBuffer.h"
+#include "CRpnCalculator.h"
 
 int main ()
 {
     // TODO: Add your program code here
-	cout << "3.2_ExtensionAnRPNCalculator started." << endl << endl;
+	cout << "3.2_ExtensionAnRPNcalc started." << endl << endl;
 
 	    CLifoBuffer buffer(5); // Create a buffer with 5 elements
 
@@ -30,5 +29,37 @@ int main ()
 
 	    buffer.print();
 
-	return 0;
+	    std::cout << " -------RPN CALCULATOR BELOW------- " << std::endl;
+
+	    CRpnCalculator calc(5); // size of 5
+	    CFraction f4(5,2), f5(6,5), f6(1,2), f7(1,3), f8(1,6), f9(1,4);
+
+		// Evaluating the numerator: (5/2 * 6/5 + 1/2 - 1/3)
+		calc.pushValue(CFraction(5, 2));
+		calc.pushValue(CFraction(6, 5));
+		calc.multiply(); // 5/2 * 6/5
+		calc.pushValue(CFraction(1, 2));
+		calc.add();      // (5/2 * 6/5) + 1/2
+		calc.pushValue(CFraction(1, 3));
+		calc.subtract(); // ((5/2 * 6/5) + 1/2) - 1/3
+
+		// Evaluating the denominator: 5 + 1/6 + 11/4
+		calc.pushValue(CFraction(5, 1));
+		calc.pushValue(CFraction(1, 6));
+		calc.add();      // 5 + 1/6
+		calc.pushValue(CFraction(11, 4));
+		calc.add();      // (5 + 1/6) + 11/4
+
+		// Final division: numerator / denominator
+		calc.divide();   // ((5/2 * 6/5 + 1/2 - 1/3) / (5 + 1/6 + 11/4))
+
+		// Pop and print the result
+		CFraction result;
+		if (calc.popValue(result)) {
+			std::cout << "Result: " << result << std::endl;
+		} else {
+			std::cerr << "Error: failed to pop result from calc." << std::endl;
+		}
+
+		return 0;
 }
