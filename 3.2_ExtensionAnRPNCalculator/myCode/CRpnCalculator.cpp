fc52e16425afde51a65e35190bc1a5fe7e6543a0
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-07-11 15:10:24 +0200

    changed variable names

--- 3.2_ExtensionAnRPNCalculator/myCode/CRpnCalculator.cpp
+++ 3.2_ExtensionAnRPNCalculator/myCode/CRpnCalculator.cpp
@@ -21,11 +21,11 @@ bool CRpnCalculator::popValue(CFraction &data)
 
 void CRpnCalculator::add()
 {
-	CFraction f1, f2, sum;
+	CFraction f1, f2, result;
 	if(m_stack.pop(f1) && m_stack.pop(f2))
 	{
-		sum = f1 + f2;
-		m_stack.push(sum);
+		result = f1 + f2;
+		m_stack.push(result);
 	}
 	else
 	{
@@ -35,11 +35,11 @@ void CRpnCalculator::add()
 
 void CRpnCalculator::subtract()
 {
-	CFraction f1, f2, sum;
+	CFraction f1, f2, result;
 	if(m_stack.pop(f1) && m_stack.pop(f2))
 	{
-		sum = f2 - f1; //order matters
-		m_stack.push(sum);
+		result = f2 - f1; //order matters
+		m_stack.push(result);
 	}
 	else
 	{
@@ -49,11 +49,11 @@ void CRpnCalculator::subtract()
 
 void CRpnCalculator::multiply()
 {
-	CFraction f1, f2, sum;
+	CFraction f1, f2, result;
 	if(m_stack.pop(f1) && m_stack.pop(f2))
 	{
-		sum = f1 * f2; //order matters
-		m_stack.push(sum);
+		result = f1 * f2; //order matters
+		m_stack.push(result);
 	}
 	else
 	{
@@ -63,13 +63,13 @@ void CRpnCalculator::multiply()
 
 void CRpnCalculator::divide()
 {
-	CFraction f1, f2, sum;
+	CFraction f1, f2, result;
 	if(m_stack.pop(f1) && m_stack.pop(f2))
 	{
 		if(f1.toDouble() != 0.0)
 		{
-			sum = f2 / f1;
-			m_stack.push(sum);
+			result = f2 / f1;
+			m_stack.push(result);
 		}
 		else
 		{
