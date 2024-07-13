f16b514ab1d7b38bcb95f1137d836020ef775dd6
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-07-13 20:05:15 +0200

    declared most methods - check account creation and deletion

--- 3.3_BankSimulation/myCode/Bank.h
+++ 3.3_BankSimulation/myCode/Bank.h
@@ -8,11 +8,36 @@
 #ifndef BANK_H_
 #define BANK_H_
 
+#include <string>
+#include <map>
+#include <memory>
+#include <Poco/Data/Date.h>
+
+
+#include "Customer.h"
+#include "Account.h"
+
 class Bank
 {
+private:
+
+	unsigned int maxNoOfCustomers;
+	unsigned int maxNoOfAccounts;
+	unsigned int maxNoOfTransactions;
+
+	std::map<unsigned int, Customer> customers;
+	std::map<unsigned int, std::unique_ptr<const Account>> accounts;
+
 public:
-	Bank();
-	virtual ~Bank();
+
+	Bank(unsigned int maxNoOfCustomers, unsigned int maxNoOfAccounts, unsigned int maxNoOfTransactions);
+	~Bank();
+
+	void addCustomer(std::string firstName, std::string lastName); //TODO check this
+	void deleteCustomer(std::string firstName, std::string lastName); //TODO check this
+	void performTransaction(unsigned int sourceAccountId, unsigned int targetAccountId, double amount, Poco::Data::Date transactionId);
+	void GenerateReport();
+
 };
 
 #endif /* BANK_H_ */
