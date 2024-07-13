24511b25c3bbd3231faf2ea732a7cb02bdd1f9e6
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-07-13 20:21:57 +0200

    declared methods

--- 3.3_BankSimulation/myCode/Account.h
+++ 3.3_BankSimulation/myCode/Account.h
@@ -8,11 +8,26 @@
 #ifndef ACCOUNT_H_
 #define ACCOUNT_H_
 
+
+
 class Account
 {
+
+private:
+
+	static unsigned int accountID;
+	double balance;
+	//Customer customer; //TODO very account belongs to a user..?? check if this is the correct way
+
+
+
 public:
 	Account();
-	virtual ~Account();
+	~Account();
+
+	double getBalance() const;
+	void setBalance(double balance);
+
 };
 
 #endif /* ACCOUNT_H_ */
