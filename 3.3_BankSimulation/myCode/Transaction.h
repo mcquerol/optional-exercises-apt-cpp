0a23767308126c71d9e455cfcc36a86766faffa8
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-07-13 21:07:14 +0200

    declared methods

--- 3.3_BankSimulation/myCode/Transaction.h
+++ 3.3_BankSimulation/myCode/Transaction.h
@@ -8,11 +8,32 @@
 #ifndef TRANSACTION_H_
 #define TRANSACTION_H_
 
+#include <Poco/Data/Date.h>
+
+#include "Account.h"
+
 class Transaction
 {
+private:
+
+	double amount = 0.0;
+	Account* sourceAccount, targetAccount;
+	Poco::Data::Date transactionDate;
+
 public:
-	Transaction();
-	virtual ~Transaction();
+
+	Transaction(const Account* sourceAccount, const Account* targetAccount, double amount);
+	~Transaction();
+
+
+	void performTransaction(unsigned int sourceAccountId, unsigned int targetAccountId, double amount, Poco::Data::Date transactionId);
+
+	double getTransactionAmount() const;
+	Account* getSourceAccount() const;
+	Account* getTargetAccount() const;
+	Poco::Data::Date getTransactionDate() const;
+
+
 };
 
 #endif /* TRANSACTION_H_ */
