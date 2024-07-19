9f077be6c1cce95b810ab66d482ee63975dd6226
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-07-19 17:24:45 +0200

    changed accounts and transactions to pointer vectors

--- 3.3_BankSimulation/myCode/Customer.h
+++ 3.3_BankSimulation/myCode/Customer.h
@@ -1,18 +1,32 @@
-/*
- * Customer.h
- *
- *  Created on: 11 Jul 2024
- *      Author: mateo
- */
-
 #ifndef CUSTOMER_H_
 #define CUSTOMER_H_
 
-class Customer
-{
+#include <string>
+#include <vector>
+#include "Account.h"
+#include "Transaction.h"
+
+class Customer {
+private:
+    std::string firstName;
+    std::string lastName;
+    std::vector<Account*> accounts; // Vector of Account pointers
+    std::vector<Transaction*> transactions;
+
 public:
-	Customer();
-	virtual ~Customer();
+    Customer();
+    Customer(const std::string& firstName, const std::string& lastName);
+    ~Customer();
+
+    void createAccount(double initialBalance = 0.0);
+    void deleteAccount(unsigned int accountId);
+    void addTransaction(Transaction* transaction);
+
+    const std::vector<Transaction*>& getTransactions() const;
+    const std::vector<Account*>& getAccounts() const; // Correct the return type
+
+    std::string getFirstName() const;
+    std::string getLastName() const;
 };
 
 #endif /* CUSTOMER_H_ */
