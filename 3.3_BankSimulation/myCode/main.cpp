9307e02d32661e92f6b76bcf06d4017be9e5bde6
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2024-07-19 17:25:13 +0200

    added test cases

--- 3.3_BankSimulation/myCode/main.cpp
+++ 3.3_BankSimulation/myCode/main.cpp
@@ -1,19 +1,66 @@
-// Standard (system) header files
+/*
+ * main.cpp
+ *
+ *  Created on: 11 Jul 2024
+ *      Author: mateo
+ */
+
 #include <iostream>
-#include <cstdlib>
-// Add more standard header files as required
-// #include <string>
+#include "Bank.h"
 
-using namespace std;
-
-// Add your project's header files here
-// #include "CFraction.h"
-
-// Main program
-int main ()
+int main()
 {
-    // TODO: Add your program code here
-	cout << "3.3_BankSimulation started." << endl << endl;
+    Bank bank(100, 100, 100);
 
-	return 0;
+    // Adding customers
+    bank.addCustomer("Paul", "Panther");
+    bank.addCustomer("Whoopie", "Goldberg");
+
+    // Creating accounts for customers
+    auto& customers = bank.getCustomers();
+
+    customers[1000].createAccount(100);
+    customers[1001].createAccount(100);
+
+    // Performing some transactions
+    Poco::DateTime date(1998,01,12);
+    bank.performTransaction(100000, 100001, 50.0, date);
+
+
+    // Generating reports after each step
+    bank.generateReport();
+
+    // Trying to create an account for a non-existing customer
+    // This will throw an error because customer ID 1002 does not exist
+    if (customers.find(1002) != customers.end()) {
+        customers[1002].createAccount(0.0);
+    } else {
+        std::cerr << "Customer ID 1002 does not exist." << std::endl;
+    }
+    // Generating reports after each step
+    bank.generateReport();
+
+    // Performing some transactions
+    Poco::DateTime date2(2012,02,12);
+    bank.performTransaction(100000, 100001, 50.0, date2);
+
+    // Generating reports after each step
+    bank.generateReport();
+
+    // Deleting an account
+    if (customers.find(1000) != customers.end()) {
+        customers[1000].deleteAccount(100000);
+    } else {
+        std::cerr << "Customer ID 1000 does not exist." << std::endl;
+    }
+    // Generating reports after each step
+    bank.generateReport();
+
+    // Deleting a customer
+    bank.deleteCustomer(1000);
+
+    // Generating reports after each step
+    bank.generateReport();
+
+    return 0;
 }
