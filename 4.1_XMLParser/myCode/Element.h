d9e8cfd81ec4cc3f5aebf21db1ab88763c59ae5c
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-08-10 14:14:59 +0200

    declare methods for element class

--- 4.1_XMLParser/myCode/Element.h
+++ 4.1_XMLParser/myCode/Element.h
@@ -8,11 +8,27 @@
 #ifndef ELEMENT_H_
 #define ELEMENT_H_
 
+#include "Node.h"
+
+#include <string>
+
 class Element
 {
+
+private:
+	Node* m_content[10];
+	int m_contentNodes;
+	std::string m_tag;
+
+	bool addToContent(Node* child);
+	bool parseStartOrEndTag(const std::string& input, unsigned int& parsePosition, bool& isStartTag, std::string& tag);
+
 public:
+
 	Element();
 	~Element();
+	bool parseInput(const std::string& input, unsigned int& parsePosition);
+	Node::node_t getNodeType() const;
 };
 
 #endif /* ELEMENT_H_ */
