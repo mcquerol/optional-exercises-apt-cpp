975c0e9d36cf956a31da59c19b9f8a27bc36da0b
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-08-10 19:56:47 +0200

    implement methods

--- 4.1_XMLParser/myCode/Text.cpp
+++ 4.1_XMLParser/myCode/Text.cpp
@@ -7,14 +7,32 @@
 
 #include "Text.h"
 
-Text::Text()
+Text::Text() : Node(Node::node_t::TEXT)
 {
-	// TODO Auto-generated constructor stub
-
 }
 
-Text::~Text()
+std::string Text::getText() const
 {
-	// TODO Auto-generated destructor stub
+	return m_text;
 }
 
+void Text::setText(std::string text)
+{
+	m_text = text;
+}
+
+bool Text::parseInput(const std::string &input, unsigned int &parsePosition)
+{
+	return false; //dummy return
+	//TODO implement this method
+}
+
+void Text::print(int indent) const
+{
+	//TODO implement this method
+}
+
+Node::node_t Text::getNodeType() const
+{
+	return Node::getNodeType();
+}
