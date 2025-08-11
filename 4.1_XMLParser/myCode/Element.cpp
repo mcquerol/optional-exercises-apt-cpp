d5db9bc4edb4953e91982aca4cb2c856c6482e64
Mateo Ceballos Querol <mateo.cebquerol@gmail.com>
2025-08-11 12:03:09 +0200

    implement cnosutrctor and destructor

--- 4.1_XMLParser/myCode/Element.cpp
+++ 4.1_XMLParser/myCode/Element.cpp
@@ -7,14 +7,44 @@
 
 #include "Element.h"
 
-Element::Element()
+Element::Element() : Node{Node::node_t::ELEMENT} , m_contentNodes{0}, m_tag{""}
 {
-	// TODO Auto-generated constructor stub
+
+	for(int i = 0; i < 10; i++)
+	{
+		m_content[i] = nullptr;
+	}
 
 }
 
 Element::~Element()
 {
-	// TODO Auto-generated destructor stub
+	for(int i = 0; i < m_contentNodes; i++)
+	{
+		delete m_content[i];
+	}
 }
 
+bool Element::addToContent(Node *child)
+{
+	return false; //dummy return
+	//TODO implment method
+}
+
+bool Element::parseStartOrEndTag(const std::string &input, unsigned int &parsePosition, bool &isStartTag, std::string &tag)
+{
+	return false; //dummy return
+	//TODO implement method
+}
+
+
+bool Element::parseInput(const std::string &input, unsigned int &parsePosition)
+{
+	return false; //dummy return
+	//TODO implement method
+}
+
+Node::node_t Element::getNodeType() const
+{
+	return Node::getNodeType();
+}
